f8f5a17ad293d3065b3d7fbfd9cc06788ea4237a
ks1451828 <ks1451828@gmail.com>
2023-12-07 23:52:39 +0530

    password generator using password class

---
+++ js/Alarm_Clock.js
@@ -0,0 +1,5 @@
+function play() {
+    var audio = new Audio('https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3');
+    audio.play();
+  }
+  play()
---
+++ js/Pass_Gen.js
@@ -0,0 +1,36 @@
+//create a js program to generating a password which contains atleast one lowercase,one uppercase and one special character.create a password class to achieve the same
+
+class Password{
+ 
+    constructor()
+    {
+        console.log("welcome to password generator")
+        this.pass=""
+    }
+    generatePassword(len)
+    {
+        let chars="abcdefghijklmnopqrstuvwxyz"
+        let numbers="1234567890"
+        let special="!@#$%^&*()"
+        if(len<3)
+        {
+           console.log("your password should be atleast 3 character long") 
+        }
+        else{
+            let i=0
+            while(i<len)
+                {
+                    this.pass += chars[Math.floor(Math.random()*(chars.length))];
+                    this.pass += numbers[Math.floor(Math.random()*(numbers.length))];
+                    this.pass += special[Math.floor(Math.random()*(special.length))];
+                    i+=3
+                }
+                this.pass = this.pass.substr(0,len)
+                return this.pass
+        }
+
+    }
+}
+//object of class Password
+let p=new Password()
+console.log(p.generatePassword(7));
